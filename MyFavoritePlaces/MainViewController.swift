f2ee24d1c30c928c7253c34d772d1f3ac4208b2d
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-05-03 01:09:42 +0400

    Add: Prepare for Sorting

--- MyFavoritePlaces/MainViewController.swift
+++ MyFavoritePlaces/MainViewController.swift
@@ -8,8 +8,10 @@
 import UIKit
 import RealmSwift
 
-class MainViewController: UITableViewController {
-    
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+  @IBOutlet weak var tableView: UITableView!
+
   var places: Results<Place>!
 
     override func viewDidLoad() {
@@ -22,12 +24,12 @@ class MainViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       return  places.isEmpty ? 0 : places.count
     }
 
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
 
         let place = places[indexPath.row]
@@ -43,8 +45,8 @@ class MainViewController: UITableViewController {
         return cell
     }
 
-  // MARK: Teble view delegate
-  override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+  // MARK: Table view delegate
+  func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
 
     let place = places[indexPath.row]
     let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (_, _) in
